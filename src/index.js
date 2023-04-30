d452375bd02f21ae599fbfd25b82c92dcb03a807
ExtraAngel <39832901+ExtraAngel@users.noreply.github.com>
2023-04-30 13:35:58 +0000

    link to event.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Main from "./Main";
 import AboutUS from "./AboutUS";
+import Events from "./Events"
 
 //When Needed add another route to get the link function
 export default function App() {
