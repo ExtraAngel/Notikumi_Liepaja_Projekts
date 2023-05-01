2b0503b7128e7673e8c30f8a4b2793b72b54d038
ExtraAngel <39832901+ExtraAngel@users.noreply.github.com>
2023-05-01 13:19:01 +0300

    Add files via upload

---
+++ src/NavBar.js
@@ -0,0 +1,55 @@
+//import React from "react";
+//import "./styles.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+/*function Navbar() {
+  return (
+    <nav>
+      <div className="nav-nosaukums">Notikumi Liepājā</div>
+      <div className="nav-saites">
+        <ul>
+          <li>
+            <Link to="/Main">Aktuālais</Link>
+          </li>
+          <li>
+            <Link to="/Events">Saraksts</Link>
+          </li>
+          <li>
+            <Link to="/AboutUS">Par mums</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;*/
+import React from "react";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navigation-bar">
+      <div className="title">Notikumi Liepājā</div>
+      <div className="buttons">
+        <button>
+          <Link to="/Main" className="normal-link">
+            Aktuālais
+          </Link>
+        </button>
+        <button>
+          <Link to="/Events" className="normal-link">
+            Saraksts
+          </Link>
+        </button>
+        <button>
+          <Link to="/AboutUS" className="normal-link">
+            Par mums
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
