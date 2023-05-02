813668fbf410bfbbe1cf60cdade2016b7f3a205a
Gustavs22 <129301330+Gustavs22@users.noreply.github.com>
2023-05-02 21:41:02 +0300

    slideTing SlideTing

--- src/SlideTing.js
+++ src/SlideTing.js
@@ -1,31 +1,71 @@
 import React from "react";
 import { Carousel } from "antd";
 
-const contentStyle: React.CSSProperties = {
-  height: "160px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "#364d79",
-  margin: 0
-};
-
 export default function SlideTing() {
+  /*const contentStyle = {
+    margin: 0,
+    height: "200px",
+    color: "#fff",
+    lineHeight: "340px",
+    textAlign: "center",
+    background: "#364d79",
+    verticalAlign: "center"
+  };*/
+
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
   return (
     <>
       <div>
-        <Carousel autoplay>
+        <Carousel autoplay afterChange={onChange} className="ant-carousel">
           <div>
-            <h3 style={contentStyle}>1</h3>
+            <h3>
+              <iframe
+                title="Joke"
+                frameborder="0"
+                src="https://th.bing.com/th/id/OIP.Gxhn2g67-uC_X7Jg0SSSGwHaEo?w=276&h=180&c=7&r=0&o=5&pid=1.7"
+                allowFullScreen
+                className="slick-slide-img"
+              ></iframe>
+              <div>1</div>
+            </h3>
           </div>
           <div>
-            <h3 style={contentStyle}>2</h3>
+            <h3>
+              <iframe
+                title="Joke"
+                frameborder="0"
+                src="https://th.bing.com/th/id/OIP.Gxhn2g67-uC_X7Jg0SSSGwHaEo?w=276&h=180&c=7&r=0&o=5&pid=1.7"
+                allowFullScreen
+                className="slick-slide-img"
+              ></iframe>
+              <div>2</div>
+            </h3>
           </div>
           <div>
-            <h3 style={contentStyle}>3</h3>
+            <h3>
+              <iframe
+                title="Joke"
+                frameborder="0"
+                src="https://th.bing.com/th/id/OIP.Gxhn2g67-uC_X7Jg0SSSGwHaEo?w=276&h=180&c=7&r=0&o=5&pid=1.7"
+                allowFullScreen
+                className="slick-slide-img"
+              ></iframe>
+              <div>3</div>
+            </h3>
           </div>
           <div>
-            <h3 style={contentStyle}>4</h3>
+            <h3>
+              <iframe
+                title="Joke"
+                frameborder="0"
+                src="https://th.bing.com/th/id/OIP.Gxhn2g67-uC_X7Jg0SSSGwHaEo?w=276&h=180&c=7&r=0&o=5&pid=1.7"
+                allowFullScreen
+                className="slick-slide-img"
+              ></iframe>
+              <div>4</div>
+            </h3>
           </div>
         </Carousel>
       </div>
