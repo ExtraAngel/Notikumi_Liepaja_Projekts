efac975c27b31aea08914f56f1112a31dd97c129
ExtraAngel <39832901+ExtraAngel@users.noreply.github.com>
2023-05-03 06:37:40 +0000

    Changed to new img

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,6 @@
 import "./styles.css";
 import { Carousel } from "antd";
 import SlideTing from "./SlideTing";
-//import BoxTing from "./BoxTing";
 import BoxMain from "./BoxMain";
 
 //HOMEPAGE
@@ -10,9 +9,9 @@ export default function Main() {
   return (
     <>
       <div className="App">
-        <div>
-          <SlideTing />
-        </div>
+
+          <img src="img/reklama.jpg" alt="Reklama"/>
+
         <div>
           {BoxMain(0)}
           {BoxMain(1)}
